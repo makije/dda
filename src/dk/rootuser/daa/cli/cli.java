e9366318bf212c266031c22e7c48a4904f6d6bf7
Martin Kiforenko Jensby <martin@rootuser.dk>
2013-04-13 16:10:03 +0200

    Parse upto title, displaying the whole data set onto the console

--- src/dk/rootuser/daa/cli/cli.java
+++ src/dk/rootuser/daa/cli/cli.java
@@ -1,12 +1,16 @@
 package dk.rootuser.daa.cli;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
 import dk.rootuser.daa.parsers.DataCiteParser;
+import dk.rootuser.daa.pojos.datacite.Creator;
+import dk.rootuser.daa.pojos.datacite.Resource;
+import dk.rootuser.daa.pojos.datacite.Title;
 
 public class cli {
 
@@ -15,6 +19,8 @@ public class cli {
 	 */
 	public static void main(String[] args) {
 
+		ArrayList<Resource> resources = new ArrayList<Resource>();
+		
 		DataCiteParser parser = null;
 		
 		try {
@@ -28,12 +34,33 @@ public class cli {
 		}
 		
 		try {
-			parser.parse(args[0]);
+			
+			for(String s : args)
+				resources.add(parser.parse(s));
+			
 		} catch (SAXException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		int resourceNumber = 0;
+		
+		for(Resource r : resources) {
+			System.out.println("Resource Number: " + resourceNumber);
+			System.out.println("Identifier: " + r.getIdentifier().getIdentifier() + " Type: " + r.getIdentifier().getIdentifierType());
+			System.out.println("Creators");
+			for(Creator c : r.getCreators())
+				System.out.println("\t" + c.getName());
+			
+			System.out.println("Titles");
+			for(Title t : r.getTitles())
+				System.out.println("\t\"" + t.getTitle() + "\"" + ( t.getTitleType() != null ? " (" + t.getTitleType() + ")" : "" ));
+			
+			System.out.println();
+			resourceNumber++;
+		}
+		
 	}
 
 }
--- src/dk/rootuser/daa/handlers/DataCiteHandler.java
+++ src/dk/rootuser/daa/handlers/DataCiteHandler.java
@@ -1,32 +1,124 @@
 package dk.rootuser.daa.handlers;
 
+import java.util.ArrayList;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import dk.rootuser.daa.handlerhelpers.DataCiteHandlerHelper;
+import dk.rootuser.daa.pojos.datacite.Creator;
+import dk.rootuser.daa.pojos.datacite.Identifier;
 import dk.rootuser.daa.pojos.datacite.Resource;
+import dk.rootuser.daa.pojos.datacite.Title;
 
 public class DataCiteHandler extends DefaultHandler {
 
 	private Resource resource = null;
 	
+	private Attributes attributes;
+	
+	private boolean isInResource = false;
+	
+	private boolean isInIdentifier = false;
+	
+	private boolean isInCreators = false;
+	private boolean isInCreator = false;
+	private boolean isInCreatorName = false;
+	
+	private boolean isIntitles;
+	private boolean isIntitle;
+	
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-		System.out.println("uri: " + uri + "\tlocalName: " + localName + "\tqName: " + qName);
+//		System.out.println("uri: " + uri + "\tlocalName: " + localName + "\tqName: " + qName);
+//		
+//		for(int i = 0; i < attributes.getLength(); i++) {
+//			System.out.println("\tAttrib: " + i + "\turi: " + attributes.getURI(i) + "\tlocalName: " + attributes.getLocalName(i) + "\tqName: " + attributes.getQName(i));
+//		}
 		
-		for(int i = 0; i < attributes.getLength(); i++) {
-			System.out.println("\tAttrib: " + i + "\turi: " + attributes.getURI(i) + "\tlocalName: " + attributes.getLocalName(i) + "\tqName: " + attributes.getQName(i));
+		this.attributes = attributes; 
+		
+		String tag = (localName == null || localName.length() == 0 ? qName : localName);
+		
+		if(tag.equals(DataCiteHandlerHelper.RESOURCE_TAG)) {
+			isInResource = true;
+			resource = new Resource();
+		} else if(tag.equals(DataCiteHandlerHelper.IDENTIFIER_TAG)) {
+			isInIdentifier = true;
+		} else if(tag.equals(DataCiteHandlerHelper.CREATORS_TAG)) {
+			isInCreators = true;
+			resource.setCreators(new ArrayList<Creator>());
+		} else if(tag.equals(DataCiteHandlerHelper.CREATOR_TAG)) {
+			isInCreator = true;
+		} else if(tag.equals(DataCiteHandlerHelper.CREATOR_NAME_TAG)) {
+			isInCreatorName = true;
+		} else if(tag.equals(DataCiteHandlerHelper.TITLES_TAG)) {
+			isIntitles = true;
+			resource.setTitles(new ArrayList<Title>());
+		} else if(tag.equals(DataCiteHandlerHelper.TITLE_TAG)) {
+			isIntitle = true;
 		}
+		
 	}
 	
 	@Override
 	public void characters(char[] ch, int start, int length) throws SAXException {
-
+		if(isInResource) {
+			
+			if(isInIdentifier) {
+				Identifier i = new Identifier();
+				i.setIdentifier(new String(ch, start, length));
+				i.setIdentifierType(attributes.getValue(attributes.getIndex(DataCiteHandlerHelper.IDENTIFIER_TYPE)));
+				
+				resource.setIdentifier(i);
+			} else if(isInCreators) {
+				if(isInCreator) {
+					if(isInCreatorName) {
+						Creator c = new Creator();
+						c.setName(new String(ch, start, length));
+						resource.getCreators().add(c);
+					}
+				}
+			} else if(isIntitles) {
+				if(isIntitle) {
+					int titleType;
+					Title t = new Title();
+					t.setTitle(new String(ch, start, length));
+					if((titleType = attributes.getIndex(DataCiteHandlerHelper.TITLE_TYPE)) >= 0)
+						t.setTitleType(attributes.getValue(titleType));
+					resource.getTitles().add(t);
+				}
+			}
+			
+		}
+		
+		
+//		for(int i = 0; i < attributes.getLength(); i++) {
+//			System.out.println("\tAttrib: " + i + "\turi: " + attributes.getURI(i) + "\tlocalName: " + attributes.getLocalName(i) + "\tqName: " + attributes.getQName(i));
+//		}
 	}
 
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
-
+		
+		String tag = (localName == null || localName.length() == 0 ? qName : localName);
+		
+		if(tag.equals(DataCiteHandlerHelper.RESOURCE_TAG)) {
+			isInResource = false;
+		} else if(tag.equals(DataCiteHandlerHelper.IDENTIFIER_TAG)) {
+			isInIdentifier = false;
+		} else if(tag.equals(DataCiteHandlerHelper.CREATORS_TAG)) {
+			isInCreators = false;
+		} else if(tag.equals(DataCiteHandlerHelper.CREATOR_TAG)) {
+			isInCreator = false;
+		} else if(tag.equals(DataCiteHandlerHelper.CREATOR_NAME_TAG)) {
+			isInCreatorName = false;
+		} else if(tag.equals(DataCiteHandlerHelper.TITLES_TAG)) {
+			isIntitles = false;
+		} else if(tag.equals(DataCiteHandlerHelper.TITLE_TAG)) {
+			isIntitle = false;
+		}
 	}
 	
 	public Resource getParsedResource() {
