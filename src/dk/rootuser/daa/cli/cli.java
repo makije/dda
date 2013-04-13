1a5766d213986f7a6b3ec9d6dd725a2f7a8d1b9d
Martin Kiforenko Jensby <martin@rootuser.dk>
2013-04-13 18:16:09 +0200

    Only show the first title and description

--- src/dk/rootuser/daa/cli/cli.java
+++ src/dk/rootuser/daa/cli/cli.java
@@ -8,20 +8,12 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
 
 import dk.rootuser.daa.parsers.DataCiteParser;
-import dk.rootuser.daa.pojos.datacite.AlternateIdentifier;
-import dk.rootuser.daa.pojos.datacite.Creator;
-import dk.rootuser.daa.pojos.datacite.Date;
-import dk.rootuser.daa.pojos.datacite.Description;
-import dk.rootuser.daa.pojos.datacite.Format;
 import dk.rootuser.daa.pojos.datacite.Resource;
-import dk.rootuser.daa.pojos.datacite.Size;
-import dk.rootuser.daa.pojos.datacite.Subject;
-import dk.rootuser.daa.pojos.datacite.Title;
 
 public class cli {
 
 	/**
-	 * @param args
+	 * @param args contains the arguments 
 	 */
 	public static void main(String[] args) {
 
@@ -50,75 +42,10 @@ public class cli {
 			System.exit(-4);
 		}
 		
-		int resourceNumber = 0;
-		
 		for(Resource r : resources) {
-			System.out.println("Resource Number: " + resourceNumber);
-			System.out.println("Identifier: " + r.getIdentifier().getIdentifier() + " Type: " + r.getIdentifier().getIdentifierType());
-			
-			if(r.getCreators() != null) {
-				System.out.println("Creators");
-				for(Creator c : r.getCreators())
-					System.out.println("\t" + c.getName());
-			}
-			
-			if(r.getTitles() != null) {
-				System.out.println("Titles");
-				for(Title t : r.getTitles())
-					System.out.println("\t\"" + t.getTitle() + "\"" + ( t.getTitleType() != null ? " (Type: " + t.getTitleType() + ")" : "" ));
-			}
-			
-			System.out.println("Publisher: " + r.getPublisher());
-			System.out.println("Publication year: " + r.getPublicationYear());
-			
-			if(r.getSubjects() != null) {
-				System.out.println("Subjects");
-				for(Subject s : r.getSubjects())
-					System.out.println("\t\"" + s.getSubject() + "\"" + ( s.getSubjectScheme() != null ? " (Scheme: " + s.getSubjectScheme() + ")" : "" ));
-			}
-			
-			if(r.getDates() != null) {
-				System.out.println("Dates");
-				for(Date d : r.getDates())
-					System.out.println("\t" + d.getDate() + ( d.getDateType() != null ? " (DateType: " + d.getDateType() + ")" : "" ));
-			}
-			
-			System.out.println("Language: " + r.getLanguage());
-			
-			if(r.getResourceType() != null) {
-				System.out.println("ResourceType: " + r.getResourceType().getResourceType() + (r.getResourceType().getResourceTypeGeneral() != null ? " (ResourceTypeGeneral: " + r.getResourceType().getResourceTypeGeneral() + ")" : "" ));
-			}
-			
-			if(r.getAlternateIdentifiers() != null) {
-				System.out.println("AlternateIdentifiers");
-				for(AlternateIdentifier a : r.getAlternateIdentifiers())
-					System.out.println("\t" + a.getAlternateIdentifier() + (a.getAlternateIdentifierType() != null ? " (Type: " + a.getAlternateIdentifierType() + ")" : "" ));
-			}
-			
-			if(r.getSizes() != null) {
-				System.out.println("Sizes");
-				for(Size s : r.getSizes())
-					System.out.println("\t" + s.getSize());
-			}
-			
-			if(r.getFormats() != null) {
-				System.out.println("Formats");
-				for(Format f : r.getFormats())
-					System.out.println("\t" + f.getFormat());
-			}
-			
-			System.out.println("Version: " + r.getVersion());
-			
-			System.out.println("Rights: " + r.getRights());
-			
-			if(r.getDescriptions() != null) {
-				System.out.println("Description");
-				for(Description d : r.getDescriptions())
-					System.out.println("\t" + d.getDescription() + (d.getDescriptionType() != null ? " (Type: " + d.getDescriptionType() + ")" : "" ));
-			}
-			
+			System.out.println(r.getTitles().get(0).getTitle());
+			System.out.println("\t" + r.getDescriptions().get(0).getDescription());
 			System.out.println();
-			resourceNumber++;
 		}
 		
 	}
