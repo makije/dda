898407c4a21cf2de95a2b69df00fd4bb874b9865
Martin Kiforenko Jensby <martin@rootuser.dk>
2013-04-13 18:47:47 +0200

    Change the cli accoring to the new features

--- src/dk/rootuser/daa/cli/cli.java
+++ src/dk/rootuser/daa/cli/cli.java
@@ -2,6 +2,7 @@ package dk.rootuser.daa.cli;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -9,16 +10,26 @@ import org.xml.sax.SAXException;
 
 import dk.rootuser.daa.parsers.DataCiteParser;
 import dk.rootuser.daa.pojos.datacite.Resource;
+import dk.rootuser.daa.sorters.SortOrder;
+import dk.rootuser.daa.sorters.TitleSorter;
 
 public class cli {
 
+	private static ArrayList<String> filesToParse = new ArrayList<String>();
+	private static SortOrder titleSortOrder = SortOrder.NONE;
+	
+	private static long parsingStarted = 0;
+	private static long parsingTook = 0;
+	private static long sortingStarted = 0;
+	private static long sortingTook = 0;
+	
 	/**
 	 * @param args contains the arguments 
 	 */
 	public static void main(String[] args) {
 
 		if(args.length == 0) {
-			System.out.println("Please provide some files to me");
+			System.out.println("-h for help");
 			System.exit(-1);
 		}
 		
@@ -29,17 +40,55 @@ public class cli {
 		try {
 			parser = DataCiteParser.getInstance();
 			
-			for(String s : args)
-				resources.add(parser.parse(s));
+			for(String s : args) {
+				if(s.contains(".xml")) {
+					filesToParse.add(s);
+				} else {
+					if(s.equalsIgnoreCase("title.asc"))
+						titleSortOrder = SortOrder.ASC;
+					else if(s.equalsIgnoreCase("title.desc"))
+						titleSortOrder = SortOrder.DESC;
+				}
+			}
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 			System.exit(-2);
 		} catch (SAXException e) {
 			e.printStackTrace();
 			System.exit(-3);
+		}
+		
+		System.out.println("you gave me " + filesToParse.size() + " files to crunch");
+		if(titleSortOrder != SortOrder.NONE)
+			System.out.println("Sorting the titles " + (titleSortOrder == SortOrder.ASC ? "ascending" : "descending"));
+		else
+			System.out.println("No sorting is being applied");
+		System.out.println("Press enter to start");
+		
+		try {
+			System.in.read();
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.exit(-4);
+		}
+		
+		parsingStarted = System.currentTimeMillis();
+		
+		for(String s : filesToParse)
+			try {
+				resources.add(parser.parse(s));
+			} catch (SAXException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		
+		parsingTook = System.currentTimeMillis() - parsingStarted;
+		
+		if(titleSortOrder != SortOrder.NONE) {
+			sortingStarted = System.currentTimeMillis();
+			TitleSorter titleSorter = new TitleSorter(titleSortOrder);
+			Collections.sort(resources, titleSorter);
+			sortingTook = System.currentTimeMillis() - sortingStarted;
 		}
 		
 		for(Resource r : resources) {
@@ -48,6 +97,7 @@ public class cli {
 			System.out.println();
 		}
 		
+		System.out.println("\nIt took me " + parsingTook + "ms to parse" + (titleSortOrder != SortOrder.NONE ? " and sorting was " + sortingTook + "ms" : ""));
 	}
 
 }
