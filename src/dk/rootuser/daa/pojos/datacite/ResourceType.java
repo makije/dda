08c397dd7fdc7958f4af90ce0ff4302dae6a3b0e
Martin Kiforenko Jensby <martin@rootuser.dk>
2013-04-13 17:24:50 +0200

    Add the ResourceType class

---
+++ src/dk/rootuser/daa/pojos/datacite/ResourceType.java
@@ -0,0 +1,24 @@
+package dk.rootuser.daa.pojos.datacite;
+
+public class ResourceType {
+
+	private String resourceType;
+	private String resourceTypeGeneral;
+
+	public String getResourceType() {
+		return resourceType;
+	}
+
+	public void setResourceType(String resourceType) {
+		this.resourceType = resourceType;
+	}
+
+	public String getResourceTypeGeneral() {
+		return resourceTypeGeneral;
+	}
+
+	public void setResourceTypeGeneral(String resourceTypeGeneral) {
+		this.resourceTypeGeneral = resourceTypeGeneral;
+	}
+	
+}
