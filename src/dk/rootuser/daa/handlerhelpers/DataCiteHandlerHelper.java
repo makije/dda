bff96af8d601ce91d0a864ead990d67fb515bdc8
Martin Kiforenko Jensby <martin@rootuser.dk>
2013-04-13 16:08:25 +0200

    Add a helper for the TAGs, so the code is easier to read

---
+++ src/dk/rootuser/daa/handlerhelpers/DataCiteHandlerHelper.java
@@ -0,0 +1,18 @@
+package dk.rootuser.daa.handlerhelpers;
+
+public class DataCiteHandlerHelper {
+
+	public static final String RESOURCE_TAG = "resource";
+	
+	public static final String IDENTIFIER_TAG = "identifier";
+	public static final String IDENTIFIER_TYPE = "identifierType";
+	
+	public static final String CREATORS_TAG = "creators";
+	public static final String CREATOR_TAG = "creator";
+	public static final String CREATOR_NAME_TAG = "creatorName";
+	
+	public static final String TITLES_TAG = "titles";
+	public static final String TITLE_TAG = "title";
+	public static final String TITLE_TYPE = "titleType";
+	
+}
